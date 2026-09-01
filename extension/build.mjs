38ef42ac5986243bc6323dcb1f640058a3d253c9
Judio <60542617+Judiooo@users.noreply.github.com>
2026-09-01 20:38:24 +1000

    Include history adapter in extension build

--- extension/build.mjs
+++ extension/build.mjs
@@ -12,28 +12,18 @@ await fs.cp(source, dist, { recursive: true });
 
 const indexPath = path.join(dist, "index.html");
 let html = await fs.readFile(indexPath, "utf8");
-html = html
-  .replace(/href="\/([^"]+)"/g, 'href="$1"')
-  .replace(/src="\/([^"]+)"/g, 'src="$1"')
-  .replace('<script src="app.js"></script>', '<script type="module" src="extension-bootstrap.js"></script>');
+html = html.replace(/href="\/([^"]+)"/g, 'href="$1"').replace(/src="\/([^"]+)"/g, 'src="$1"').replace('<script src="app.js"></script>', '<script type="module" src="extension-bootstrap.js"></script>');
 await fs.writeFile(indexPath, html);
 
-// app.js contains absolute asset URLs because the self-hosted build is served
-// from the Express root. Make those URLs extension-relative.
 const appJsPath = path.join(dist, "app.js");
 let appJs = await fs.readFile(appJsPath, "utf8");
-appJs = appJs
-  .replaceAll("'/icons/", "'icons/")
-  .replaceAll('"/icons/', '"icons/')
-  .replaceAll("`/icons/", "`icons/")
-  .replaceAll("'/wallpaper/", "'wallpaper/")
-  .replaceAll('"/wallpaper/', '"wallpaper/')
-  .replaceAll("`/wallpaper/", "`wallpaper/");
+appJs = appJs.replaceAll("'/icons/", "'icons/").replaceAll('"/icons/', '"icons/').replaceAll("`/icons/", "`icons/").replaceAll("'/wallpaper/", "'wallpaper/").replaceAll('"/wallpaper/', '"wallpaper/').replaceAll("`/wallpaper/", "`wallpaper/");
 await fs.writeFile(appJsPath, appJs);
 
 await fs.copyFile(path.join(here, "manifest.json"), path.join(dist, "manifest.json"));
 await fs.copyFile(path.join(here, "service-worker.js"), path.join(dist, "service-worker.js"));
 await fs.copyFile(path.join(here, "storage.js"), path.join(dist, "storage.js"));
+await fs.copyFile(path.join(here, "history.js"), path.join(dist, "history.js"));
 await fs.copyFile(path.join(here, "extension-bootstrap.js"), path.join(dist, "extension-bootstrap.js"));
 
 console.log(`Hemma extension built at ${dist}`);
